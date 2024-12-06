6ed1ea4f89d18991b4cc1fc0561a882b6cabb7c3
potcham <bcerronr@gmail.com>
2024-12-06 10:48:54 -0500

    feat: add new entities

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, UploadFile, File
+from urllib.request import URLopener
 import os
 from services.pdf_reader import extract_text_with_pymupdf
 from services.llm_handler import extract_invoice_data_with_items
 
 app = FastAPI()
 
-@app.post("/extract")
+@app.post("/extract-invoice-file")
 async def extract_invoice(file: UploadFile = File(...)):
     # Guardar archivo temporal
     temp_file = f"/tmp/{file.filename}"
@@ -25,3 +26,30 @@ async def extract_invoice(file: UploadFile = File(...)):
     os.remove(temp_file)
 
     return data
+
+
+@app.post("/extract-invoice-url")
+async def extract_invoice(url: str):
+    # Guardar archivo temporal
+    temp_file = "/tmp/file.pdf"
+    
+    try:
+        opener = URLopener()
+        opener.retrieve(url, temp_file)
+    except Exception as e:
+        print(e)
+
+    # Extraer texto del PDF
+    invoice_text = extract_text_with_pymupdf(temp_file)
+
+    # Usar el modelo LLM para estructurar los datos
+    try:
+        data = extract_invoice_data_with_items(invoice_text)
+    except Exception as e:
+        return {"error": str(e)}
+
+    # Limpiar archivo temporal
+    os.remove(temp_file)
+
+    return data
+
--- app/services/llm_handler.py
+++ app/services/llm_handler.py
@@ -1,4 +1,3 @@
-# from langchain.llms import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
@@ -25,8 +24,29 @@ def extract_invoice_data_with_items(invoice_text: str) -> dict:
     # Prompt afinado
     prompt = PromptTemplate(
         template=("""
-        Eres un experto en análisis de facturas. Extrae información clave de la siguiente factura y devuélvela como JSON en este formato:
+        Eres un experto en análisis de facturas. Extrae información clave de la siguiente factura y devuélvela como un JSON string siguiendo este formato:
         {format_instructions}
+
+        Las claves del JSON son:
+        - Número de factura (numero_factura)
+        - RUC del proveedor (ruc_proveedor)
+        - Orden de compra (orden_compra)
+        - Número de guía (numero_guia)
+        - Fecha de emisión (fecha_emision)
+        - Fecha de vencimiento (fecha_vencimiento)
+        - Número de cuota (numero_cuota)
+        - Moneda (moneda)
+        - Forma de pago (forma_pago)
+        - Condición de pago (condicion_pago)
+        - Subtotal
+        - Impuestos
+        - Monto total
+        - Ítems: lista con descripción, cantidad, precio unitario, subtotal
+
+        NOTAS IMPORTANTES:
+        - No incluyas información del cliente conocida (RUC: 20100163471, NOMBRE: JJC CONTRATISTAS GENERALES S.A, DIRECCIÓN: AV. ALFREDO BENAVIDES...).
+        - Asegúrate de capturar el RUC del proveedor correctamente, omitiendo el RUC del cliente.
+        - Si algún dato no está disponible, utiliza un string vacío ("") o `null` para valores numéricos.
 
         Texto de la factura:
         {invoice_text}
@@ -36,25 +56,18 @@ def extract_invoice_data_with_items(invoice_text: str) -> dict:
     )
 
     # Crear la cadena con LangChain
-    chain = prompt | llm # LLMChain(llm=llm, prompt=prompt)
-    # structured_data = chain.invoke({"invoice_text": text})
+    chain = prompt | llm 
 
-    # Ejecutar el LLM con el texto de la factura
-    result = chain.invoke({"invoice_text": invoice_text})
+    try:
+        # Ejecutar el LLM con el texto de la factura
+        result = chain.invoke({"invoice_text": invoice_text})
 
-    print(result.content)
+        print(result.content)
 
-    # Validar y parsear la salida al modelo Pydantic
-    parsed_invoice = invoice_parser.parse(result.content)
+        # Validar y parsear la salida al modelo Pydantic
+        parsed_invoice = invoice_parser.parse(result.content)
 
-    return parsed_invoice
-
-    # # Convertir el resultado a un diccionario
-    # # print(structured_data)
-    # print(structured_data.content)
-    # # print(structured_data["text"])
-
-    # output = json.loads(structured_data.content)
-    # print("output_format: ", output)
-
-    # return eval(structured_data)
+        return parsed_invoice.model_dump()
+    except Exception as e:
+        print(f"Error durante la extracción: {e}")
+        return Invoice().model_dump()
